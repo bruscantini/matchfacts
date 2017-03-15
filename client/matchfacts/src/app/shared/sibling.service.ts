9eeb194a86f4d9b1fe516b603ab77c21173a928f
Anthony Bruscantini <ajbruscantini@gmail.com>
2017-03-15 23:39:35 +0100

    turnovers and personal fouls are no longer 'better' if more

--- client/matchfacts/src/app/shared/sibling.service.ts
+++ client/matchfacts/src/app/shared/sibling.service.ts
@@ -8,40 +8,14 @@ export class SiblingService {
   private actualPlayer2Data: Object;
 
   // Observable sources
-  // private selectedFilter1 = new Subject<String>();
-  // private selectedFilter2 = new Subject<String>();
-  // private player1 = new Subject<Object>();
-  // private player2 = new Subject<Object>();
   private player1Data = new Subject<Object>();
   private player2Data = new Subject<Object>();
 
   // Observable streams
-  // selectedFilter1$ = this.selectedFilter1.asObservable();
-  // selectedFilter2$ = this.selectedFilter2.asObservable();
-  // changedPlayer1$ = this.player1.asObservable();
-  // changedPlayer2$ = this.player2.asObservable();
   changedPlayer1Data$ = this.player1Data.asObservable();
   changedPlayer2Data$ = this.player2Data.asObservable();
 
   constructor() { }
-
-  // setFilter(componentId: number, selectedFilter: string) {
-  //   if (componentId === 1 && this.actualPlayer1Data) {
-  //     this.selectedFilter1.next(selectedFilter);
-  //   } else {
-  //     this.selectedFilter2.next(selectedFilter);
-  //   }
-  //
-  //   // greenify the winners
-  // }
-
-  // playerChange(componentId: number, player: Object) {
-  //   if (componentId === 1) {
-  //     this.player1.next(player);
-  //   } else {
-  //     this.player2.next(player);
-  //   }
-  // }
 
   convertToDisplayableForm(playerData) {
     const keys = Object.keys(playerData);
@@ -65,20 +39,26 @@ export class SiblingService {
   // called when playerData or filter changes.
   private compareStats() {
     if (this.actualPlayer1Data && this.actualPlayer2Data) {
-      // do comparison by modifying actualPlayerData and send both;
-      console.log('we must now compare');
+      // do comparison by modifying actualPlayerData and send both
       const fields = Object.keys(this.actualPlayer1Data);
       fields.forEach((field) => {
         if (!(field in this.actualPlayer1Data) || !(field in this.actualPlayer2Data)) {
           console.log('somebody is missing a field');
           return;
         }
+
+        // TOV exception
+        let WINNER = true;
+        if (field.toLowerCase() === 'tov' || field.toLowerCase() === 'pf') {
+          WINNER = false;
+        }
+
         if (this.actualPlayer1Data[field]['value'] > this.actualPlayer2Data[field]['value']) {
-          this.actualPlayer1Data[field]['win'] = true;
-          this.actualPlayer2Data[field]['win'] = false;
+          this.actualPlayer1Data[field]['win'] = WINNER;
+          this.actualPlayer2Data[field]['win'] = !WINNER;
         } else {
-          this.actualPlayer2Data[field]['win'] = true;
-          this.actualPlayer1Data[field]['win'] = false;
+          this.actualPlayer2Data[field]['win'] = WINNER;
+          this.actualPlayer1Data[field]['win'] = !WINNER;
         }
       });
       this.player1Data.next(this.actualPlayer1Data);
