58b34870be46fade5598f70137ff8787de6aa602
Anthony Bruscantini <ajbruscantini@gmail.com>
2017-03-22 17:34:22 -0400

    implemented tooltip which expands startField abbreviations

--- client/matchfacts/src/app/comparison/comparison.component.ts
+++ client/matchfacts/src/app/comparison/comparison.component.ts
@@ -11,6 +11,32 @@ import { SiblingService } from '../shared/sibling.service';
 })
 export class ComparisonComponent implements OnInit {
   statFields: String[];
+  statAbbreviations = {
+    'gp': 'Games Played',
+    'gs': 'Games Started',
+    'min': 'Minutes',
+    'fgm': 'Field Goals Made',
+    'fga': 'Field Goals Attempted',
+    'fgPct': 'Field Goal %',
+    'fG3M': '3-point FG Made',
+    'fG3A': '3-point FG Attempted',
+    'fg3Pct': '3-point FG %',
+    'ftm': 'Free Throws Made',
+    'fta': 'Free Throws Attempted',
+    'ftPct': 'Free Throw %',
+    'oreb': 'Offensive Rebounds',
+    'dreb': 'Defensive Rebounds',
+    'reb': 'Rebounds',
+    'ast': 'Assists',
+    'stl': 'Steals',
+    'blk': 'Blocks',
+    'tov': 'Turnovers',
+    'pf': 'Personal Fouls',
+    'pts': 'Points'
+  };
+
+  // shooterStats = [ ];
+
 
   constructor(private nbaAPIService: NbaAPIService, private siblingService: SiblingService) {
     this.statFields = [
--- client/matchfacts/src/app/shared/sibling.service.ts
+++ client/matchfacts/src/app/shared/sibling.service.ts
@@ -55,6 +55,7 @@ export class SiblingService {
       // do comparison by modifying actualPlayerData and send both
       const fields = Object.keys(this.actualPlayer1Data);
       fields.forEach((field) => {
+        //field = field.toLowerCase();
         if (!(field in this.actualPlayer1Data) || !(field in this.actualPlayer2Data)) {
           console.log('somebody is missing a field');
           // we might need to return the data with missing field. else, it doesn't
